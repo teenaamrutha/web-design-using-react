718d5a3e724dbdc2eb4940f390304ad978ce01f8
teenaamrutha <68224321+teenaamrutha@users.noreply.github.com>
2020-08-26 23:40:26 +0530

    Task3

---
+++ react/react.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './example' import Props in Function from './Example Protein';
+import Class State from "./classeState";
+import FuncHook from "./functionHook" Import data from "./data/data.json" 
+import Resume from './Resume';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+function App()
+{
+return
+<div className="App">
+{/* <Card></Card> */}
+<BrowserRouter> <Route exact
+path="/" component={Card}/>
+Route exact þath="/resume component=(Resume)/
+</Browser Router>
+</div>
+}
---
+++ react/reactresume.js
@@ -0,0 +1,19 @@
+import data from /data/data.json
+let Resume (props)=>{
+let information-data.profiles(props.location.sameCardData.id);
+console.log(information);
+
+<div className="row">
+<div className="col-lg-3 m-1">
+<div className="card">
+<div className="card-body">
+<h1>(information.details.name)</h1>
+</div>
+</div>
+</div>
+<div classname="col-1 g-8 m-1">
+<h2>Career Object:</h2>
+<hr />
+<p> information.career </p>
+</div>
+}
